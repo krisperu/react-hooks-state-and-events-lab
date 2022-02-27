48684467d9bb28e6d97f839c992283ef40ba436e
Kristina Peru <krissy.germanov@gmail.com>
2022-02-26 20:21:15 -0700

    App && Item Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [category, setCategory] = useState()
   return (
     <div className="ShoppingList">
       <div className="Filter">
